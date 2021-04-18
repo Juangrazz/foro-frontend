d079e3875ecec9beb9c1e32e42002b7d915aab0b
Juan Gracia <juangrazz@hotmail.com>
2021-04-18 08:41:46 +0200

    Chaged chart js library. Added rounded bar.

--- src/app/components/views/statistics/statistics.component.ts
+++ src/app/components/views/statistics/statistics.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
+import { ChartOptions, ChartType, ChartDataset } from 'chart.js';
 import { Label } from 'ng2-charts';
 import { DatabaseService } from '../../../services/database.service';
 
@@ -24,13 +24,14 @@ export class StatisticsComponent implements OnInit {
   barChartTypeAllTime: ChartType = 'bar';
   barChartLegendAllTime = false;
   barChartPluginsAllTime = [];
-  barChartDataAllTime: ChartDataSets[] = [
+  barChartDataAllTime: ChartDataset[] = [
     {
       data: [],
       borderWidth: 0,
       backgroundColor: "rgba(0, 170, 228, .5)",
       hoverBackgroundColor: "rgba(2, 147, 196)",
       hoverBorderColor: "rgb(0, 103, 138)",
+      borderRadius: 15
     }
   ];
 
@@ -40,14 +41,14 @@ export class StatisticsComponent implements OnInit {
   barChartTypeThirtyDays: ChartType = 'bar';
   barChartLegendThirtyDays = false;
   barChartPluginsThirtyDays = [];
-  barChartDataThirtyDays: ChartDataSets[] = [
+  barChartDataThirtyDays: ChartDataset[] = [
     {
       data: [],
       borderWidth: 0,
       backgroundColor: "rgba(98, 219, 165, .5)",
       hoverBackgroundColor: "rgba(81, 180, 136)",
       hoverBorderColor: "rgb(65, 146, 110)",
-      radius: 20
+      borderRadius: 15
     }
   ];
 
@@ -57,7 +58,7 @@ export class StatisticsComponent implements OnInit {
   lineChartTypeSevenDaysCards: ChartType = 'line';
   lineChartLegendSevenDaysCards = true;
   lineChartPluginsSevenDaysCards = [];
-  lineChartDataSevenDaysCards: ChartDataSets[] = [
+  lineChartDataSevenDaysCards: ChartDataset[] = [
     {
       data: [],
       label: keys.statistic_cards_seven_days_normal_label,
@@ -66,7 +67,8 @@ export class StatisticsComponent implements OnInit {
       borderColor: 'rgb(109, 44, 125)',
       pointBackgroundColor: "rgb(109, 44, 125)",
       pointHoverBorderColor: "rgb(158, 106, 17)",
-      lineTension: .2
+      tension: .2,
+      fill: true
     },
     {
       data: [],
@@ -76,7 +78,8 @@ export class StatisticsComponent implements OnInit {
       borderColor: 'rgb(255, 102, 102)',
       pointBackgroundColor: "rgb(255, 102, 102)",
       pointHoverBorderColor: "rgb(158, 106, 17)",
-      lineTension: .2
+      tension: .2,
+      fill: true
     }
   ];
 
@@ -117,12 +120,15 @@ export class StatisticsComponent implements OnInit {
     await this.databaseService.getStatisticsPlacesThirtyDays()
       .then(resp => {
         let maxCounter = 0;
-        for (const statistics of resp) {
+        for (const statistics of resp) {  
           this.barChartLabelsThirtyDays.push(statistics.title);
           this.barChartDataThirtyDays[0].data?.push(statistics.quantity);
 
           if (statistics.quantity > maxCounter) maxCounter = statistics.quantity;
         }
+
+        console.log( this.barChartDataThirtyDays[0]);
+        
 
         this.barChartOptionsThirtyDays = this.createBarChartOptions(this.barChartOptionsThirtyDays, maxCounter)
       })
@@ -157,13 +163,18 @@ export class StatisticsComponent implements OnInit {
     barChartOptions = {
       responsive: true,
       scales: {
-        yAxes: [{
+        y: {
+          beginAtZero: true,
+          max: maxYAxes + 1,
           ticks: {
             stepSize: 1,
-            min: 0,
-            max: maxYAxes + 1
           }
-        }]
+        }
+      },
+      plugins: {
+        legend: {
+          display: false,
+        }
       }
     }
 
@@ -174,13 +185,13 @@ export class StatisticsComponent implements OnInit {
     lineChartOptions = {
       responsive: true,
       scales: {
-        yAxes: [{
+        y: {
+          beginAtZero: true,
+          max: maxYAxes + 1,
           ticks: {
             stepSize: 1,
-            min: 0,
-            max: maxYAxes + 1
           }
-        }]
+        }
       }
     }
 
